f4b5daa7d76f188f7e910e8cdf51c141ae71a20d
Afrizal Sebastian <sebastiangurning@gmail.com>
2023-11-08 22:55:02 +0700

    feat: get article

--- controller/articles.js
+++ controller/articles.js
@@ -12,7 +12,7 @@ const postArticleHandler = async (req, res, next) => {
     const postDate = new Date().toDateString();
 
 
-    const newArticleRef = articlesRef.doc();
+    const newArticleRef = articlesRef.doc(`${uniqueId}`);
     await newArticleRef.set({
       _id: uniqueId,
       title,
@@ -33,4 +33,48 @@ const postArticleHandler = async (req, res, next) => {
   }
 }
 
-module.exports = postArticleHandler
+const getAllArtilcesHandler = async (req, res, next) => {
+  try{
+    const articlesRef = db.collection('articles');
+    const snapshot = await articlesRef.get();
+
+    const data = snapshot.docs.map(doc => ({
+      _id: doc.data()._id,
+      postDate: doc.data().postDate,
+      title: doc.data().title,
+    }))
+
+    res.status(200).json({
+      status: "Success",
+      data
+    })
+    
+  }catch(error){
+    res.status(error.status || 500).json({
+      status: "Error",
+      message: error.message
+    })
+  }
+}
+
+const getArticleByIdHandler = async (req, res, next) => {
+  try{
+    const id = req.params.id;
+    const articlesRef = db.collection('articles').doc(`${id}`);
+    const doc = await articlesRef.get();
+    const data = doc.data();
+
+    res.status(200).json({
+      status: "Success",
+      data
+    })
+    
+  }catch(error){
+    res.status(error.status || 500).json({
+      status: "Error",
+      message: error.message
+    })
+  }
+}
+
+module.exports = { postArticleHandler, getAllArtilcesHandler, getArticleByIdHandler }
--- routes/articles.js
+++ routes/articles.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const postArticleHandler = require('../controller/articles');
+const { postArticleHandler, getAllArtilcesHandler, getArticleByIdHandler } = require('../controller/articles');
 
 router.post('/', postArticleHandler);
+router.get('/', getAllArtilcesHandler);
+router.get('/:id', getArticleByIdHandler);
 
 module.exports = router;
