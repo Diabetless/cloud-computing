2b2c176851a4f41092599046182d5ec081ffa370
DimasPramantya <igkdimas@gmail.com>
2023-11-29 11:57:32 +0700

    decoded is stored in request

--- middleware/user_verif.js
+++ middleware/user_verif.js
@@ -1,3 +1,8 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+const jwtKey = process.env.JWT_KEY;
+
 const getToken = (headers) => {
   const authorizationHeader = headers.authorization;
   if (authorizationHeader && authorizationHeader.startsWith('Bearer ')) {
@@ -13,8 +18,8 @@ const getToken = (headers) => {
 const userVerification = async(req,res,next)=>{
   try {
     const token = getToken(req.headers);
-    req.token = token;
-    next();
+    const decoded = jwt.verify(token, jwtKey);
+    req.decoded = decoded;
   } catch (error) {
     res.status(error.status || 500).json({
       status: "Error",
