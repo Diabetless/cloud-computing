36810836cab6ffb5fa66967a615036fc8b5c859a
Afrizal Sebastian <82313717+afrizalsebastian@users.noreply.github.com>
2023-12-10 20:15:39 +0700

    Feat: delete bmi data and blood sugar data (#8)

--- util/connect_storage.js
+++ util/connect_storage.js
@@ -3,7 +3,7 @@ const { Storage } = require('@google-cloud/storage');
 
 const storage = new Storage({
   projectId: process.env.PROJECT_ID,
-  keyFilename: './storage-credentials.json'
+  keyFilename: '../storage-credentials.json'
 })
 
 const bucket = storage.bucket(process.env.BUCKET_NAME);
